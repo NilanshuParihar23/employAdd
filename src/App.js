783d64c1396558f350e502d52dbcd6b8202ad171
NilanshuParihar23 <nilanshu0612@gmail.com>
2024-03-21 16:09:07 +0530

    added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import ContactList from './components/Contacts_view';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './style.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div className='container'>
+        <h1 className='app'>My Contact App</h1>
+        <ContactList/>
+        
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
